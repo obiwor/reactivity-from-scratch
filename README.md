07e212733b641ce3fe650ad9663be95b8db2a41d
Singfield <louisrigaudpro@gmail.com>
2023-02-26 18:17:48 +0100

    Create README.md

---
+++ README.md
@@ -0,0 +1,18 @@
+# reactivity-from-scratch
+
+
+I just read this [article](https://dev.to/emnudge/from-scratch-reactive-values-c9h) as introduction
+but some of the things explained here are confuse for me, and I want to get even more in reactivity too.
+
+this article don't speack about reactivity with proxy object.
+
+I think reactivity is a great subject and It will be a great tools for future projects. (tails ?)
+
+Finally,
+
+My plan is to have a basic understanding of how implement reactivity in vanilla JS. With that I'll compare it with the vue js code implementation
+and after maybe create my lib in another language like Rust.
+
+note :
+
+WeakSet interable ?
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { Reactor } from "./reactor.js";
 import {create, get} from "./framework.js"
 
 const num1 = new Reactor(0);
-
+const sentence = new Reactor("")
 const inputOptions = {
     rejectOn : isNaN, 
     mutator : Number
@@ -13,7 +13,8 @@ const inputOptions = {
 
 const input1 = create("input").bind('value', num1, inputOptions)
 const h1 = create("h1").bind('textContent', num1)
-
+const h2 = create("h1").bind('textContent', sentence)
+const input2 = create("input").bind("value", sentence)
 score.subscribe(val =>{
     h1.textContent = `score ${val}`
 })
@@ -22,5 +23,6 @@ score.subscribe(val =>{
 get('body')
 .append(input1)
 .append(h1)
-
+.append(input2)
+.append(h2)
 
